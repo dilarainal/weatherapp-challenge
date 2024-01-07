f129f70e0a63ae7086ea044515009b15e2abdb89
dilarainal <148790405+dilarainal@users.noreply.github.com>
2024-01-07 16:55:57 -0500

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateForcast(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "o79b0b278ad643abf38d0abtfa4f526c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateForcast);
+}
+
 function citySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-bar");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search");
 searchFormElement.addEventListener("submit", citySearch);
+
+searchCity("Rio");
